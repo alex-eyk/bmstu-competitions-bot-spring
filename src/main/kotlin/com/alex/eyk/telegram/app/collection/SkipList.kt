76d81bb532a2f602db76eae51a2bb9b3bfde0f01
Alexey Kiselev <ximand931@gmail.com>
2022-03-31 19:27:28 +0300

    searcher nullability fix

--- src/main/kotlin/com/alex/eyk/telegram/app/collection/SkipList.kt
+++ src/main/kotlin/com/alex/eyk/telegram/app/collection/SkipList.kt
@@ -2,7 +2,7 @@ package com.alex.eyk.telegram.app.collection
 
 interface SkipList<T> {
 
-    fun searcher(): SkipListSearcher<T>?
+    fun searcher(): SkipListSearcher<T>
 
     /**
      * Количество линий, использующихся в списке.
