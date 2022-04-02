8dacde579499710bbd8f7c4e7654e383d7cc68f7
Alexey Kiselev <ximand931@gmail.com>
2022-04-02 21:29:32 +0300

    reg num validator

---
+++ src/main/kotlin/com/alex/eyk/telegram/app/model/validation/impl/RegNumberValidatior.kt
@@ -0,0 +1,19 @@
+package com.alex.eyk.telegram.app.model.validation.impl
+
+import com.alex.eyk.telegram.app.model.validation.Result
+import com.alex.eyk.telegram.app.model.validation.Validator
+
+class RegNumberValidatior : Validator<String> {
+
+    companion object {
+        private val REG_NUMBER_REGEX = "(\\d{3}-\\d{3}-\\d{3} \\d{2})|(.\\d{4})".toRegex()
+    }
+
+    override fun validate(item: String): Result {
+        return if (item.matches(REG_NUMBER_REGEX)) {
+            Result.Sucess
+        } else {
+            Result.Error(WrongFormatException())
+        }
+    }
+}
---
+++ src/main/kotlin/com/alex/eyk/telegram/app/model/validation/impl/WrongFormatException.kt
@@ -0,0 +1,5 @@
+package com.alex.eyk.telegram.app.model.validation.impl
+
+import com.alex.eyk.telegram.app.model.validation.ValidationException
+
+class WrongFormatException : ValidationException()
