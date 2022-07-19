faa4a27fee0030895c6e2a3983594366021c4c7e
Alexey Kiselev <ximand931@gmail.com>
2022-07-19 22:24:18 +0300

    build shell script

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i.bak "s|dictionary.directory_path=.*|dictionary.directory_path=$PWD\/src\/main\/resources\/dictionary|" xml-replies/src/main/resources/replies-processor.properties
+echo "Xml-replies configuration updated"
+
+./gradlew bootJar
